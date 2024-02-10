60c07bd3f9401800e9f6ed714edae244b048fe35
Kaua2123 <kauantnetsoares@gmail.com>
2024-02-10 09:02:46 -0300

    métodos para gerenciamento de livros

--- src/library-manager.ts
+++ src/library-manager.ts
@@ -1,13 +1,35 @@
 import { BookProtocol } from './interfaces/book-protocol';
 
 export class LibraryManager {
-  private readonly _books: BookProtocol[] = [];
+  public readonly _books: BookProtocol[] = [];
 
   addBook(book: BookProtocol): void {
     this._books.push(book);
+    console.log('Adicionando livro:', book);
   }
 
   get books(): BookProtocol[] {
+    console.log('Listando livros:');
     return this._books;
+  }
+
+  lendBook(index: number): boolean {
+    console.log('Você pegou este livro emprestado:', this._books[index].title);
+    return (this._books[index].disponibility = false);
+  }
+
+  returnBook(index: number): boolean {
+    console.log('Você devolveu este livro:', this._books[index].title);
+    return (this._books[index].disponibility = true);
+  }
+
+  searchBook(text: string): BookProtocol[] {
+    const booksFiltered = this._books.filter((book) => {
+      if (book.title.toLowerCase().includes(text.toLowerCase())) return book;
+      if (book.author.toLowerCase().includes(text.toLowerCase())) return book;
+    });
+    console.log('Resultados para:', text);
+    console.log(booksFiltered);
+    return booksFiltered;
   }
 }
